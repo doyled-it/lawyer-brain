350028799d8133940d4b9ea0b55fd214f6b9cce1
Michael Doyle <michaeldoyle1994@gmail.com>
2024-08-07 22:18:00 -0700

    Update prompts

--- lb/rag/llm/prompts.py
+++ lb/rag/llm/prompts.py
@@ -1,6 +1,16 @@
 FIVEFOUR_TEMPLATE = """
-Based on the user's message, generate a response using the context provided
-from the FiveFour podcast transcripts.
+Based on the user's message, respond using the context provided
+from the FiveFour podcast transcripts. Make sure your response is in-depth, thoughtful,
+and relevant to the user's message.
+
+Make sure to pull from as much of the context as possible to provide a response that
+is relevant to the user's message. Ensure your response is detailed, specific, and
+expressly points to the context provided.
+
+Try to create an answer that goes past surface level and really dives into the
+points that the hosts of the FiveFour podcast were making in the context provided.
+
+Use specific quotes from the hosts using the Episode Transcript Context in your message.
 
 Episode Transcript Context: {context}
 User Message: {user_message}
