4c949f297a8d412b8ec26828a020e1bc6c1c8e6a
Michael Doyle <michaeldoyle1994@gmail.com>
2024-08-17 00:35:44 -0600

    Update prompting

--- backend/lb/rag/llm/outputs.py
+++ backend/lb/rag/llm/outputs.py
@@ -2,12 +2,17 @@ from pydantic import BaseModel, Field
 
 
 class FiveFourModelOutput(BaseModel):
-    speaker: str = Field(..., description="The speaker of the transcript entry.")
+    speaker: str = Field(..., description="The speaker of the transcript entries")
     speakers_understanding: str = Field(
         ..., description="The speaker's understanding on the topic."
     )
     other_hosts_understanding: str = Field(
         ..., description="The other hosts' understanding on the topic."
+    )
+    impactful_quotes: list[str] = Field(
+        ...,
+        description="The impactful quotes from the transcript entries. What are the most "
+        "important quotes from the transcript entries?",
     )
     reasoning: str = Field(
         ...,
@@ -19,7 +24,9 @@ class FiveFourModelOutput(BaseModel):
     message: str = Field(
         ...,
         description=(
-            "The message to be displayed to the user. "
-            "Format the text as markdown text if it makes the message more readable."
+            "The message to be displayed to the user. Include impactful quotes, "
+            "reasoning, and any other relevant information."
+            "Format the text as markdown text to make the message more readable. Don't "
+            "include a message title."
         ),
     )
